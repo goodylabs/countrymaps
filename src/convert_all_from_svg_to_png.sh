5555196806f810faf54e9eaaacaac59143da6752
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2021-06-02 16:31:00 +0200

    FIX: output files without input file extension

--- src/convert_all_from_svg_to_png.sh
+++ src/convert_all_from_svg_to_png.sh
@@ -11,6 +11,7 @@ for index in ${!PATHS[*]}; do
   input_path="${PATHS[$index]}"
   for i in `${LS} -1 ${input_path}/*.svg`; do
     output_filename=`echo ${i##*/}`
+    output_filename=`echo ${output_filename%.svg}`
     output_filename="${output_path}/${output_filename}.png"
     echo "Converting ${i} to ${output_filename} ..."
     ./convert_svg_to_png.sh ${i} ${output_filename}
