7eb6fcc4eb23efbf1bc0980a19609995d314bb1c
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2022-05-24 13:42:38 +0200

    ADD: Skip when file already exists except yoyu provide --overwrite flag

--- src/convert_all_from_svg_to_png.sh
+++ src/convert_all_from_svg_to_png.sh
@@ -7,14 +7,28 @@ SED=`which sed`
 PATHS=( "svg/dark" "svg/light")
 output_path="../png"
 
+if [ "x$1" == "x--help" ]; then
+  echo "$0 [--overwrite]"
+  exit 0;
+fi
+
+if [ "x$1" == "x--overwrite" ]; then
+  is_overwrite="true"
+fi
+
 for index in ${!PATHS[*]}; do
   input_path="${PATHS[$index]}"
   for i in `${LS} -1 ${input_path}/*.svg`; do
     output_filename=`echo ${i##*/}`
     output_filename=`echo ${output_filename%.svg}`
     output_filename="${output_path}/${output_filename}.png"
-    echo "Converting ${i} to ${output_filename} ..."
-    ./convert_svg_to_png.sh ${i} ${output_filename}
+
+    if [ "$is_overwrite" != "true" ] && [ -f "$output_filename" ]; then
+      echo "Skipping file ${output_filename} as it already exists..."
+    else
+      echo "Converting ${i} to ${output_filename} ..."
+      ./convert_svg_to_png.sh ${i} ${output_filename}
+    fi
   done
 
 done
